fffbbf085e692512370f584298a0cd0902a2ec46
Jason Pilz <jasonpilz@gmail.com>
2015-12-02 10:05:57 -0700

    Bug fix - nav bar bottom border
    - create instance var to hold navBarBottomBorder
    - remove border from superview on viewWillLayoutSubviews()
    - call custom method to reset border on viewWillLayoutSubviews()

--- Turing/MasterViewController.swift
+++ Turing/MasterViewController.swift
@@ -12,6 +12,8 @@ class MasterViewController: UITableViewController, ModelDelegate {
     var detailViewController: DetailViewController? = nil
     var people = Model.sharedInstance.people
 
+    var navBorder:UIView = UIView()
+
     let tealColor = UIColor(red: 5.0/255.0, green: 194.0/255.0, blue: 209.0/255.0, alpha: 1.0)
 
     override func viewDidLoad() {
@@ -30,9 +32,7 @@ class MasterViewController: UITableViewController, ModelDelegate {
         
         // Sets the detail view controllers back button to custom teal color
         UINavigationBar.appearance().tintColor = tealColor
-        
-        //self.setNavbarBottomBorder()
-        
+                
         self.navigationController?.toolbarHidden = false
         self.navigationController?.toolbar.barTintColor = tealColor
     }
@@ -40,11 +40,10 @@ class MasterViewController: UITableViewController, ModelDelegate {
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
         super.viewWillAppear(animated)
-        self.setNavbarBottomBorder()
     }
     
     override func viewWillLayoutSubviews() {
-        // REMOVE navBorder from Navigation Controller.navbar view heirarchy...need to pull it into instance var to specify it during removal.
+        self.navBorder.removeFromSuperview()
         self.setNavbarBottomBorder()
     }
 
@@ -117,15 +116,15 @@ class MasterViewController: UITableViewController, ModelDelegate {
     // MARK: - Custom Functions
     
     func setNavbarBottomBorder() {
-        
         if let navigationController = self.navigationController {
             let navigationBar = navigationController.navigationBar
-            let navBorder: UIView = UIView(frame: CGRectMake(0, navigationBar.frame.size.height - 3, navigationBar.frame.size.width, 3))
+            self.navBorder = UIView(frame: CGRectMake(0, navigationBar.frame.size.height - 3, navigationBar.frame.size.width, 3))
             navBorder.backgroundColor = tealColor
             navBorder.opaque = true
-            self.navigationController?.navigationBar.addSubview(navBorder)
+            self.navigationController?.navigationBar.addSubview(self.navBorder)
         }
     }
+    
 }
 
     // MARK: - Extensions
